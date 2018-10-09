61de00c8746e6562867d79ad12c7c4185866c717
breeze-easy <androdev.cft6@gmail.com>
2018-10-09 06:54:23 -0400

    adding Tue work from bsm

--- python-MITx-6.00.1/iter-min-mon-payment-precise.py
+++ python-MITx-6.00.1/iter-min-mon-payment-precise.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 def minMoPayment(bal, rate):
     """
     bal - initial CC balance, float
@@ -16,36 +11,46 @@ def minMoPayment(bal, rate):
     Mo payment lower bound = Balance / 12
     Mo payment upper bound = (Balance x (1 + Monthly interest rate)**12) / 12.0
     """
-    unpaidBal=bal
     monthlyIntRate=rate/12
-    minMonthlyPayment=(int(bal/12))
-    maxMonthlyPayment=(bal*(1+monthlyIntRate)**12)/12
+    minMonthlyPayment=round((int(bal/12)),3)
+    maxMonthlyPayment=round((bal*(1+monthlyIntRate)**12)/12,3)
+    monthlyPayment=0
+    unpaidBal=1
+    whileCounter=1
     
-    
-    while unpaidBal>0: 
-        monthlyPayment=(maxMonthlyPayment+minMonthlyPayment)/2 #bi-sectional search          
+    while unpaidBal>0:  
+        unpaidBal=bal               
+        monthlyPayment=round((maxMonthlyPayment+minMonthlyPayment)/2,3) #bi-sectional search                  
         
-        for i in range(1,13):
-            print("Using monthly payment of: " + str(monthlyPayment) + "\n")          
+        for i in range(1,13):       # look for 12 months                 
             unpaidBal -= monthlyPayment
-            if unpaidBal<monthlyPayment:
+            if unpaidBal>0:
+                monInterest = unpaidBal*monthlyIntRate
+            else:
+                monInterest=0     
                 maxMonthlyPayment=monthlyPayment
                 break
-            monInterest = unpaidBal*monthlyIntRate
-            unpaidBal += monInterest
-            print("Month " + str(i) + " remaining balance: " +
-                 str(round(unpaidBal, 2)))             
-            
-        if unpaidBal>0: #shift search boundaries
-            minMonthlyPayment=monthlyPayment        
-             
+            unpaidBal = unpaidBal + monInterest
+                               
+        unpaidBal=round(unpaidBal,2)        
         
-        unpaidBal=bal
-                
+        if unpaidBal>0: #shift search boundaries
+            minMonthlyPayment=monthlyPayment  
+        else:
+            maxMonthlyPayment=monthlyPayment
+            unpaidBal=bal
+        
+        if abs(unpaidBal)<=0.01:
+            break
+        
+        whileCounter+=1  
+        print("While Loop No: " + str(whileCounter))           
+                                
     return monthlyPayment
         
     
    
+a1=320000; i1=0.2
+a2=999999; i2=0.18    
     
-    
-print("Lowest Payment: " + str(minMoPayment(3926,0.2)))
+print("Lowest Payment 1: " + str(round(minMoPayment(a2, i2),2)))
